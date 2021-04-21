faa4fc0e8fdbc55e1367a0122c59b20333993d53
Rahil Rehan <rahil.rehan@thoughtworks.com>
2021-04-21 11:39:12 +0530

    [sripriya/rahil] Add. Route, Logic and Test for multipyling two numbers

--- __tests__/getRequests.spec.js
+++ __tests__/getRequests.spec.js
@@ -15,5 +15,12 @@ describe('testing get routes', () =>{
         expect(response.headers['content-type']).toEqual('application/json; charset=utf-8')
         expect(response.data.answer).toEqual(13)
     })
+
+    test('shouldReturnValidMultiplicationForTwoNumbers', async () => {
+        const response = await axios('http://localhost:3000/calculator/multiply/4/5')
+        expect(response.status).toEqual(200)
+        expect(response.headers['content-type']).toEqual('application/json; charset=utf-8')
+        expect(response.data.answer).toEqual(20)
+    })
 })
 
