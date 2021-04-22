2145fc567912752a24454a9ae9785e025d1d4069
Rahil Rehan <rahil.rehan@thoughtworks.com>
2021-04-22 07:02:55 +0530

    [sripriya/rahil] Add. Request query according to standards

--- app/calculator.js
+++ app/calculator.js
@@ -7,22 +7,22 @@ const getOperands = (params) => {
     return Object.keys(params).map((key) => Number(params[key]))
 }
 
-router.get('/add/:operandOne/:operandTwo', (req, res) => {
-    const [operandOne, operandTwo] = getOperands(req.params)
+router.get('/add', (req, res) => {
+    const [operandOne, operandTwo] = getOperands(req.query)
     res.json({answer:operandOne + operandTwo})
 })
 
-router.get('/multiply/:operandOne/:operandTwo', (req, res, next) => {
+router.get('/multiply', (req, res, next) => {
     if(process.env.DISABLE_MULTIPLY === 'true'){
-        const [operandOne, operandTwo] = getOperands(req.params)
+        const [operandOne, operandTwo] = getOperands(req.query)
         res.json({answer:operandOne * operandTwo})
     }else{
         next()
     }
 })
 
-router.get('/divide/:operandOne/:operandTwo', (req, res) => {
-    const [operandOne, operandTwo] = getOperands(req.params)
+router.get('/divide', (req, res) => {
+    const [operandOne, operandTwo] = getOperands(req.query)
     res.json({answer:operandOne / operandTwo})
 })
 
