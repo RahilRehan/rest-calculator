162ba11b08a5945bed88badcd79868df312b8187
Rahil Rehan <rahil.rehan@thoughtworks.com>
2021-04-26 20:55:48 +0530

    [sripriya/rahil] Add. testcases for calculator api

--- __tests__/calculator-api.spec.js
+++ __tests__/calculator-api.spec.js
@@ -1,4 +1,3 @@
-const { query } = require('express')
 const add = require('../calculator/add'),
     multiply = require('../calculator/multiply'),
     divide = require('../calculator/divide')
@@ -8,5 +7,15 @@ describe('test calculator api', () => {
         const query = {a:23, b:44}
         answer = add(query)
         expect(answer).toEqual(67)
+    })
+    test('shouldReturnValidValueForMultiply', () => {
+        const query = {a:20, b:10}
+        answer = multiply(query)
+        expect(answer).toEqual(200)
+    })
+    test('shouldReturnValidValueForDivide', () => {
+        const query = {a:50, b:10}
+        answer = divide(query)
+        expect(answer).toEqual(5)
     })
 })
--- __tests__/get-requests.spec.js
+++ __tests__/get-requests.spec.js
@@ -11,21 +11,26 @@ describe('testing get routes', () =>{
         const response = await axios('http://localhost:3000/calculator/add/?a=6&b=7')
         expect(response.status).toEqual(200)
         expect(response.headers['content-type']).toEqual('application/json; charset=utf-8')
-        expect(response.data.answer).toEqual(13)
     })
 
     test('shouldReturnValidMultiplicationForTwoNumbers', async () => {
         const response = await axios('http://localhost:3000/calculator/multiply/?a=4&b=5')
         expect(response.status).toEqual(200)
         expect(response.headers['content-type']).toEqual('application/json; charset=utf-8')
-        expect(response.data.answer).toEqual(20)
     })
 
     test('shouldReturnValidDivisionForTwoNumbers', async () => {
         const response = await axios('http://localhost:3000/calculator/divide/?a=10&b=2')
         expect(response.status).toEqual(200)
         expect(response.headers['content-type']).toEqual('application/json; charset=utf-8')
-        expect(response.data.answer).toEqual(5)
+    })
+    test('temptest', async () => {
+        try{
+            const response = await axios('http://localhost:3000/calculator/divide/?a=10')
+        }catch(e){
+            expect(e.response.data.status).toEqual(404)
+            expect(e.response.headers['content-type']).toEqual('application/json; charset=utf-8')
+        }
     })
 
 })
